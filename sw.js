3a6798c96e973878950309f8e31671c688a0b7d0
mateusz <mistrzumati@spoko.pl>
2022-05-03 17:49:49 +0200

    Add dist folder

---
+++ sw.js
@@ -0,0 +1,39 @@
+importScripts('/_nuxt/workbox.4c4f5ca6.js')
+
+workbox.precaching.precacheAndRoute([
+  {
+    "url": "/_nuxt/0abf672.js",
+    "revision": "7f1ac3dd097ef87bff4f3133401a6898"
+  },
+  {
+    "url": "/_nuxt/c04a15d.js",
+    "revision": "1d639bd87e3fca6ea966cfc12fa8adf8"
+  },
+  {
+    "url": "/_nuxt/cc5e257.js",
+    "revision": "22f672f92c957927e38241ecb55fb0bc"
+  },
+  {
+    "url": "/_nuxt/e0c571f.js",
+    "revision": "e5407fe9235a366bafb44c7257da25a4"
+  },
+  {
+    "url": "/_nuxt/e298e2e.js",
+    "revision": "660afc9f4e09f26f0858f8884837a47b"
+  },
+  {
+    "url": "/_nuxt/e39d00a.js",
+    "revision": "4be42cca1c2d94a1d174388209d71aca"
+  }
+], {
+  "cacheId": "Stilko",
+  "directoryIndex": "/",
+  "cleanUrls": false
+})
+
+workbox.clientsClaim()
+workbox.skipWaiting()
+
+workbox.routing.registerRoute(new RegExp('/_nuxt/.*'), workbox.strategies.cacheFirst({}), 'GET')
+
+workbox.routing.registerRoute(new RegExp('/.*'), workbox.strategies.networkFirst({}), 'GET')
